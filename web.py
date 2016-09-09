9100e2665c9b13df8c0f2495ade7bc2bb8440658
Rodrigo Barriuso <rodrigo.barriuso@gmail.com>
2016-09-09 11:22:32 +0200

    web.py

--- web.py
+++ web.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+import os
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -10,4 +12,5 @@ def about():
 	return render_template('about.hmtl')
 	
 if __name__ == "__main__":
-    app.run()
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
