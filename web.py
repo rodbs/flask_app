6862c5a03c3f0c5dadc09dfd20e1c2c9ef87c573
Rodrigo Barriuso <rodrigo.barriuso@gmail.com>
2016-09-09 11:29:01 +0200

    Update web.py

--- web.py
+++ web.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index():	
     return render_template('index.html')
 
-@app.route('/about')
+@app.route("/about")
 def about():
-	return render_template('about.hmtl')
+    return render_template('about.html')
 	
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
